57f6e74071451cf26cd2dd7df82508f7c60dd5f0
Nick Kosarev <hmbanan666@hotmail.com>
2025-07-18 12:26:42 +0200

    fix: browser problem

--- apps/web-parser/server/utils/error.ts
+++ apps/web-parser/server/utils/error.ts
@@ -1,16 +1,8 @@
-import { type } from 'arktype'
 import { H3Error } from 'h3'
 
 const logger = useLogger('error')
 
 export function errorResolver(exception: unknown) {
-  if (exception instanceof type.errors) {
-    throw createError({
-      statusCode: 400,
-      message: exception.summary,
-    })
-  }
-
   if (exception instanceof H3Error) {
     throw exception
   }
